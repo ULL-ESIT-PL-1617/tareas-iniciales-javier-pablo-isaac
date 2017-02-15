cc493dfa38893dea417766cdc98f143449b7ceb0
Isaac Aiman <alu0100841565@ull.edu.es>
2017-02-15 18:41:31 +0000

    Updates

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var ghpages = require('gh-pages');
-var path = require('path');
-
-gulp.task('deploy', function() {
-ghpages.publish(path.join(__dirname, '_book'), function(err) {
-      console.log('publicado en gh-pages');
-    });
-  });
